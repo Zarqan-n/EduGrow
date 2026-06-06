b9921880560b062d93e6b4e647730919b8440849
Zarqan <zarqannazeer4567@gmail.com>
2026-06-06 22:48:13 +0530

    Base url update

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 import { store } from '../store/index'
 import { logout } from '../store/slices/authSlice'
 
-// const API_BASE_URL = "https://edugrowbackend.onrender.com/api"
-const API_BASE_URL = "http://localhost:5000/api"
+const API_BASE_URL = "https://edugrowbackend.onrender.com/api"
+// const API_BASE_URL = "http://localhost:5000/api"
 
 const api = axios.create({
   baseURL: API_BASE_URL,
