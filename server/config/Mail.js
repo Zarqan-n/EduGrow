098e2106cacbaeafe898af340bcf73505baef567
Zarqan <zarqannazeer4567@gmail.com>
2026-06-06 23:14:40 +0530

    Email issue fix

--- server/config/Mail.js
+++ server/config/Mail.js
@@ -1,12 +1,26 @@
-import nodemailer from 'nodemailer'
-import dotenv from 'dotenv'
-dotenv.config()
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log("EMAIL:", process.env.EMAIL);
+console.log("PASS EXISTS:", !!process.env.PASS);
 
 export const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
         user: process.env.EMAIL,
-        pass: process.env.PASS
-    }
-})
+        pass: process.env.PASS,
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
+});
 
+transporter.verify((error, success) => {
+    if (error) {
+        console.error("VERIFY ERROR:", error);
+    } else {
+        console.log("SMTP Ready");
+    }
+});
