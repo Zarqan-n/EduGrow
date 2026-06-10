0c1e14cdb082e82148695aedb0d7ebf10084b4a3
Zarqan <zarqannazeer4567@gmail.com>
2026-06-10 19:07:36 +0530

    ip4v problem fix

--- server/config/Mail.js
+++ server/config/Mail.js
@@ -4,20 +4,22 @@ import nodemailer from "nodemailer";
 dotenv.config()
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASS,
-  },
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASS,
+    },
 });
 
 console.log("EMAIL:", process.env.EMAIL);
 console.log("PASS LENGTH:", process.env.PASS?.length);
 
 transporter.verify((err, success) => {
-  if (err) {
-    console.error("VERIFY ERROR:", err);
-  } else {
-    console.log("SMTP READY");
-  }
+    if (err) {
+        console.error("VERIFY ERROR:", err);
+    } else {
+        console.log("SMTP READY");
+    }
 });
