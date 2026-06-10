00a29e0d32359600dd538bccd05ca06af6adab44
Zarqan <zarqannazeer4567@gmail.com>
2026-06-10 18:59:17 +0530

    DNS problem fix 2

--- server/config/Mail.js
+++ server/config/Mail.js
@@ -1,11 +1,23 @@
-import nodemailer from 'nodemailer'
 import dotenv from 'dotenv'
+import nodemailer from "nodemailer";
+
 dotenv.config()
 
 export const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASS
-    }
-})
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+});
+
+console.log("EMAIL:", process.env.EMAIL);
+console.log("PASS LENGTH:", process.env.PASS?.length);
+
+transporter.verify((err, success) => {
+  if (err) {
+    console.error("VERIFY ERROR:", err);
+  } else {
+    console.log("SMTP READY");
+  }
+});
--- server/utils/EmailSender.js
+++ server/utils/EmailSender.js
@@ -5,15 +5,17 @@ dotenv.config()
 
 export const sendEmail = async (email, subject, template) => {
     try {
+        console.log("Sending mail to:", email)
         const sendMail = await transporter.sendMail({
             from: "EduGrow",
             to: email,
             subject: subject,
             html: template
         })
-        console.log("Sendmail res: ",sendMail)
+        console.log("MAIL SENT");
+        console.log(sendMail);
     }
     catch (err) {
-       console.log(err.message)
+        console.log(err)
     }
 }
